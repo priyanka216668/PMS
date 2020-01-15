005b34816fe2c0977bd70eaf8bff8df1ae3c9230
priyanka216668 <priyanka@joshsoftware.com>
2020-01-16 00:56:38 +0530

    Updated TODO_STATUS, TOTo_TYPES

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 USER_ROLES = { admin: 'admin', developer: 'developer', manager: 'manager'}
 TEAM_CATEGORY = {mobile: 'mobile', testing: 'testing', front_end: 'front_end',  back_end: 'back_end'}
-TODO_STATUS = {new: 'new', in_progress: 'in_progress', completed: 'completed'}
-TODO_TYPES = {feature: 'feature', bug: 'bug'}
+TODO_STATUS = ['done','new','in_progress']
+TODO_TYPES = ['feature', 'bug']
